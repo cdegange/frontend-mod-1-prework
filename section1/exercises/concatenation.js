4badad0e87e1840d9b8778f380a8be17b8c78580
Chad DeGange <chaddegange@gmail.com>
2021-09-13 11:23:20 -0600

    Completed exercises

--- section1/exercises/concatenation.js
+++ section1/exercises/concatenation.js
@@ -22,11 +22,14 @@ var creatures = "unicorns";
 console.log("My zoo has " + numberOfCreatures + " " + creatures + "!")
 
 // YOU DO: What data type will be logged to the console as a result of line 22? Explain.
-// Your answer and explanation here: 
+// Your answer and explanation here:
+
+A string. The concatenation of using a variable in place of a number converts it to a string.
 
 
 // YOU DO:
 // Write code that combines the variables below into a string that
+console.log('The ' + speedy + ' jumped over the ' + 'slowPoke');
 // reads "The quick red fox jumped over the lazy brown dog":
 var speedy = "quick red fox";
 var slowPoke = "lazy brown dog";
@@ -35,6 +38,9 @@ var slowPoke = "lazy brown dog";
 // YOU DO:
 // Write code that combines the variables below into a string that
 // reads "In a predictable result, the tortoise beat the hare!"
+
+console.log('In a predictable result, the ' + slowPoke + ' beat the ' + speedy + '!');
+
 // Note that we are omitting the var keyword below, because we are re-assigning the variables
 
 slowPoke = "tortoise";
@@ -42,14 +48,20 @@ speedy = "hare";
 
 
 // YOU DO:
-// Declare three variables, name/content/data type of your choice. Think carefully about what 
+// Declare three variables, name/content/data type of your choice. Think carefully about what
   // you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!)
+
+var petName = 'River';
+var petType = 'Dog';
+var petBreed = 'German Shorthaired Pointer';
+
   // Then, log out ONE sentence that incorporates all THREE variables.
 
+console.log('My ' + petType + ' ' petName + ' is a ' + petBreed + '!');
 
 //-------------------
 // FINAL CHECK
 //-------------------
 
-// Did you run this file in your terminal to make sure everything printed out to the console 
+// Did you run this file in your terminal to make sure everything printed out to the console
   // as you would expect? You should be doing this by now, and getting into the habit of it.
